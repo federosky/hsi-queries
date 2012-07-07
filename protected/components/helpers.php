c4b52a1d316abdf3330542faab5f414e36340185
federosky <federosky@gmail.com>
2012-07-07 13:04:33 -0300

    Added updated time formatter

--- protected/components/helpers.php
+++ protected/components/helpers.php
@@ -17,16 +17,24 @@ function formatWeight($weight)
 	return $weight;
 }
 
+/**
+ * Formats race enlapse time,
+ * @param mixed $time_enlapsed
+ */
 function formatTimeEnlapsed( $time_enlapsed )
 {
 	$formatted = '0\'0\'\'0';
 	if( !empty($time_enlapsed) )
 	{
-		$part3 = substr($time_enlapsed,-3);
-		$part2 = substr($time_enlapsed,-5,2);
-		$part1 = substr($time_enlapsed,0,-5);
-
-		$formatted = $part1.'\''.$part2.'\'\''.$part3;
+		if( is_numeric($time_enlapsed) )
+		{
+			$part3 = substr($time_enlapsed,-3);
+			$part2 = substr($time_enlapsed,-5,2);
+			$part1 = substr($time_enlapsed,0,-5);
+	
+			$formatted = $part1.'\''.$part2.'\'\''.$part3;
+		}
+		else $formatted = $time_enlapsed;
 	}
 	return $formatted;
 }
